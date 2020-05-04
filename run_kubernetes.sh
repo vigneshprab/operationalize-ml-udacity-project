449e9c6da9ed75ffe62235037e943c4d723b2a16
vignesh <vigneshprab@gmail.com>
2020-05-04 12:20:12 +0200

    Review feedback files updated

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=vigneshprab/app_flask
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run app-flask --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/app-flask 8000:80 
